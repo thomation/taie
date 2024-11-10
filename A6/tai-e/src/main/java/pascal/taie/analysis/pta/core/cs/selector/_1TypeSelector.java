912fa2a5b9a3590a996751e9d8a533cf4f2203f2
Thomation <thomation@126.com>
2024-11-10 10:51:21 +0800

    Implement One Type

--- A6/tai-e/src/main/java/pascal/taie/analysis/pta/core/cs/selector/_1TypeSelector.java
+++ A6/tai-e/src/main/java/pascal/taie/analysis/pta/core/cs/selector/_1TypeSelector.java
@@ -42,19 +42,19 @@ public class _1TypeSelector implements ContextSelector {
 
     @Override
     public Context selectContext(CSCallSite callSite, JMethod callee) {
-        // TODO - finish me
-        return null;
+        // LIB6
+        return callSite.getContext();
     }
 
     @Override
     public Context selectContext(CSCallSite callSite, CSObj recv, JMethod callee) {
-        // TODO - finish me
-        return null;
+        // LIB6
+        return ListContext.make(recv.getObject().getContainerType());
     }
 
     @Override
     public Context selectHeapContext(CSMethod method, Obj obj) {
-        // TODO - finish me
-        return null;
+        // LIB6
+        return ListContext.make();
     }
 }
